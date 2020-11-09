1febf66a69e1e86e02e6eb1c2fbe32cf6cca3d2f
Naautiik <hubert.janczaak@gmail.com>
2020-11-09 11:52:43 +0100

    debugScheduler

--- Scheduler.py
+++ Scheduler.py
@@ -40,7 +40,7 @@ while True:
     if (datetime.today().weekday()) != 5 and (datetime.today().weekday()) != 6:
         print((datetime.today().weekday()))
         print("waiting")
-        if (datetime.now().strftime('%H')) == '11':
+        if (datetime.now().strftime('%H')) == '10':
             print("sending")
             f = Subskrypcje.query.all()
             print(f)
